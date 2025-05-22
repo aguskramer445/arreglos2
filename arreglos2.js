85dd5e1c0db0a1c7930d77086a1c0e2df212f989
alumno <alumno@Instituto.local>
2025-05-22 11:21:43 -0300

    Update arreglos2.js

--- arreglos2.js
+++ arreglos2.js
@@ -1,3 +1,24 @@
+//EJERCICIO 1
+
 //Punto 1
 let frutas = ["manzana", "banana", "naranja"];
 console.log(frutas);
+//Punto 2
+frutas.push("uva");
+console.log(frutas);
+//Punto 3
+frutas.unshift("frutilla");
+console.log(frutas);
+//Punto 4
+frutas.shift("frutilla");
+console.log(frutas)
+//Punto 5
+frutas.pop("uva");
+console.log(frutas)
+
+
+//EJERCICIO 2
+
+//Punto 1
+let nombres = ["Marcelo", "Ernesto", "Florinda"]
+console.log(nombres)
