b150675dc56efe50763d3f7d47d5da68cde8982c
aguskramer445 <48733741@ivt.edu.ar>
2025-05-27 18:40:06 -0300

    Update arreglos2.js

--- arreglos2.js
+++ arreglos2.js
@@ -21,4 +21,45 @@ console.log(frutas)
 
 //Punto 1
 let nombres = ["Marcelo", "Ernesto", "Florinda"]
-console.log(nombres)
+//Punto 2
+console.log("Cantidad inicial de nombres:", nombres.length);
+//Punto 3
+nombres.push("Carlos");
+nombres.push("Ana");
+//Punto 4
+console.log("Cantidad final de nombres:", nombres.length);
+
+//EJERCICIO 3
+
+//Punto 1
+let numeros = [1, 2, 3, 4, 5];
+//Punto 2
+for (let i = 0; i < numeros.length; i++) {
+  console.log(numeros[i]);
+}
+
+//EJERCICIO 4
+
+//Punto 1
+let Numeros = [5, 10, 15, 20];
+let suma = 0;
+//Punto 2
+for (let i = 0; i < Numeros.length; i++) {
+  suma += Numeros[i];
+}
+//Punto 3
+console.log("La suma total es: " + suma);
+
+//EJERCICIO 5
+
+//Punto 1
+let palabras = [];
+
+for (let i = 0; i < 5; i++) {
+  let palabra = prompt("Escribí una palabra:");
+  //Punto 2
+  palabras.push(palabra);
+}
+//Punto 3
+console.log("Las palabras ingresadas son:");
+console.log(palabras);
